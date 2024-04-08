732bbc57a848b7dcb84b9c2e35ed1352b16640e5
fujiwen <94854200+fujiwen@users.noreply.github.com>
2024-04-08 16:24:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -31,6 +31,7 @@ function scanSubdirectories(directory) {
 const themes = scanSubdirectories(path.resolve(__dirname, 'themes'))
 module.exports = withBundleAnalyzer({
   images: {
+    unoptimized: true,
     // 图片压缩
     formats: ['image/avif', 'image/webp'],
     // 允许next/image加载的图片 域名
